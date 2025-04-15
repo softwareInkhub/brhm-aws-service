eb17c610cda3d3e8b6afb9fecd91fb323f5c3ca8
softwareInkhub <software.inkhub@gmail.com>
2025-04-15 16:09:24 +0530

    updates

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 const nextConfig = {
   output: 'standalone',
   reactStrictMode: true,
+  productionBrowserSourceMaps: false,
+  compress: true,
   env: {
     AWS_REGION: process.env.AWS_REGION,
     AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
@@ -20,7 +22,7 @@ const nextConfig = {
     AWS_REGION: process.env.AWS_REGION,
     AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
     AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
-    AWS_LAMBDA_ROLE_ARN: process.env_AWS_LAMBDA_ROLE_ARN,
+    AWS_LAMBDA_ROLE_ARN: process.env.AWS_LAMBDA_ROLE_ARN,
     AWS_S3_BUCKET_NAME: process.env.AWS_S3_BUCKET_NAME,
   },
   // Add this to ensure environment variables are available at runtime
